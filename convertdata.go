8c30f59e1dee4ca287f877f3cff76fd9d38a1993
atrem13 <mertayoga17.my@gmail.com>
2022-09-01 13:44:19 +0800

    update

---
+++ convertdata.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var angkaInteger = 50
+	fmt.Println(angkaInteger)
+}
--- variable.go
+++ variable.go
@@ -3,8 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	// inisiasi variable
 	var nama string
+	// memberi nilai pada variable
 	nama = "merta yoga"
 
+	// inisiasi langsung isi nilai variable
+	var nama2 = "nama lainnya"
+
+	// contoh lainnya
+	nama3 := "ini nyoba yakk"
+
+	// inisiasi multiple variables
+	var (
+		firstName = "dwi"
+		lastName  = "permana"
+	)
+
+	// constanta
+	// wajib langsung diberi values
+	// tidak bisa diubah setelah diberi values
+	const iniConstan = "hehe"
+
 	fmt.Println(nama)
+	fmt.Println(nama2)
+	fmt.Println(nama3)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(iniConstan)
 }
