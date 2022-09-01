9ad8ea3be2183e4d022a6868abcaed2d09452d22
atrem13 <mertayoga17.my@gmail.com>
2022-09-01 14:06:31 +0800

    update

---
+++ operasimatematika.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// operasi aritmatika disini sama seperti bahasa pemrograman lainnya
+	// +,-, /, *, %, ++, --, !, dll
+	a := 10
+	b := 15
+	c := 20
+
+	fmt.Println(a + b + c)
+}
