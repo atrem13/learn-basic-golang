1403a3e2a2e943140746dccc4f4a8a9eba474609
atrem13 <mertayoga17.my@gmail.com>
2022-09-02 11:19:28 +0800

    update

---
+++ operasiboolean.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// operator booleannya sama seperti pada programming language yang lainnya
+	// &&
+	// ||
+	// !
+	nama := true
+	nama2 := false
+	fmt.Println(nama && nama2)
+	fmt.Println(nama || nama2)
+	fmt.Println(!nama)
+}
---
+++ perbandingan.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// operator perbandingannya sama seperti pada programming language yang lainnya
+	// <=
+	// >=
+	// ==
+	// !=
+	nama := "eko"
+	nama2 := "eko2"
+	fmt.Println(nama == nama2)
+	fmt.Println(nama != nama2)
+}
